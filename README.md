df22548d1646bad21268c8a2931a10a53f5fd4cb
Lucas Fernández <lucas.fernamdez.lmf11@gmail.com>
2022-01-13 16:49:10 -0300

    Proyecto terminado
    push final de poyecto terminado

---
+++ README.md
@@ -0,0 +1,60 @@
+<h1>Proyecto Clarisa Vau</h1>
+
+
+
+<h3> 👨🏻‍💻 Sobre el proyecto </h3>
+
+- 🎓 &nbsp; CoderHouse entrego como premio un Proyecto Real a los egresados pertenecientes al Top 10 de cada camada.
+- 💼 &nbsp; Sera un proyecto con un equipo conformado por dos Desarrolladores y una Diseñadora.
+- ✍️ &nbsp; Nuestra cliente es una escritora argentina de ficcion, que desea incorporar un sitio web ligado a sus redes y publicaciones.
+
+<h3> 🛠 &nbsp;Tecnologias a utilizar</h3>
+
+
+- 🌐 &nbsp;
+  ![HTML5](https://img.shields.io/badge/-HTML5-333333?style=flat&logo=HTML5)
+  ![CSS](https://img.shields.io/badge/-CSS-333333?style=flat&logo=CSS3&logoColor=1572B6)
+  ![JavaScript](https://img.shields.io/badge/-JavaScript-333333?style=flat&logo=javascript)
+  ![Bootstrap](https://img.shields.io/badge/-Bootstrap-333333?style=flat&logo=bootstrap&logoColor=563D7C) 
+ 
+  
+- ⚙️ &nbsp;
+  ![Git](https://img.shields.io/badge/-Git-333333?style=flat&logo=git)
+  ![GitHub](https://img.shields.io/badge/-GitHub-333333?style=flat&logo=github)
+  
+- 🔧 &nbsp;
+  ![Visual Studio Code](https://img.shields.io/badge/-Visual%20Studio%20Code-333333?style=flat&logo=visual-studio-code&logoColor=007ACC)
+  
+- 🖥 &nbsp;
+  ![Illustrator](https://img.shields.io/badge/-Illustrator-333333?style=flat&logo=adobe-illustrator)
+  ![Photoshop](https://img.shields.io/badge/-Photoshop-333333?style=flat&logo=adobe-photoshop)
+  
+<br/>
+
+
+<br/>
+
+<h3> 🤝🏻 &nbsp;contribuciones y contactos </h3>
+
+<p align="center">
+<h3>Lucas Fernadez</h3>
+<h5>Desarrollo</h5>
+-[linkedin] (https://www.linkedin.com/in/fernandezlucas-it/)
+
+<h3>Daniela Maschio</h3>
+<h5>Desarrollo</h5>
+-[linkedin] ()<br/>
+-[GitHub] (https://github.com/danimaschio6)
+
+<h3>Giselle Pereiras</h3>
+<h5>Diseño/marketing</h5>
+-[linkedin] (https://www.linkedin.com/in/giselle-pereiras)<br/>
+
+<h3>Clarisa Vau</h3>
+<h5>Escritora</h5>
+-[linkedin] (https://www.linkedin.com/in/valentina-sodiro-313732206/)<br/>
+-[Facebook] (http://facebook.com/ClarisaVau.Escritora))<br/>
+-[Instagram] (https://www.instagram.com/ClarisaVau.Escritora/)
+
+</p>
+
---
+++ js/main.js
@@ -0,0 +1,124 @@
+// if(document.getElementById("botonLibro1")){
+
+//     var body = document.getElementById('body') 
+
+
+//     var modal = document.getElementById("modalUno")
+//     var btn = document.getElementById("botonLibro1");
+    
+
+//     btn.onclick = function() {
+//         modal.style.display = "flex";
+//         modal.style.position = "absolute";
+//         modal.style.marginTop = "0px";
+//         modal.style.marginLeft = "190px";
+//         modal.style.marginRight = "auto";            
+//     }
+
+// }
+
+
+// function btnCerrar(){
+//     $('#modalUno').hide();
+// }
+
+
+
+// VALIDACION DE FORM
+
+
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+
+
+// expresiones regulares:
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    pais: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+    edad: /^\d{1,3}$/,
+
+}
+
+const campos = {
+	nombre: false,
+    apellido: false,
+	correo: false,
+	telefono: false,
+    pais: false,
+    edad: false,
+ 
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+        case "apellido":
+			validarCampo(expresiones.apellido, e.target, 'apellido');
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+		break;
+        case "pais":
+			validarCampo(expresiones.pais, e.target, 'pais');
+		break;
+        case "edad":
+			validarCampo(expresiones.edad, e.target, 'edad');
+		break;
+
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	
+	if(campos.apellido && campos.nombre && campos.correo && campos.telefono  && campos.pais && campos.edad ){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
